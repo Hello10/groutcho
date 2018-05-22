ad92e781f87c320c1f21f1331e2e914e876c1e3a
Stephen Handley <stephen.handley@gmail.com>
2018-05-21 17:08:23 -0700

    rename exports

---
+++ src/Router.js
@@ -0,0 +1,108 @@
+const Route = require('./Route');
+const MatchResult = require('./MatchResult');
+
+class Router {
+  constructor ({
+    routes,
+    session,
+    redirects
+  }) {
+    this.routes = [];
+    this.addRoutes(routes);
+    this.session = session;
+
+    this.redirects = {};
+    let redirect_args = [
+      'notFound',
+      'sessionMissing',
+      'sessionExisting',
+      'roleMissing'
+    ];
+    for (let attr of redirect_args) {
+      const name = redirects[attr];
+      if (!(name in routes)) {
+        throw new Error(`No route named ${name}`);
+      } else {
+        this.redirects[attr] = this.getRoute({name});
+      }
+    }
+
+    this.listeners = [];
+  }
+
+  addRoutes (routes) {
+    let entries = Object.entries(routes);
+    for (const [name, config] of entries) {
+      const {path: pattern, page, session, role} = config;
+      const route = new Route({name, pattern, page, session, role});
+      this.routes.push(route);
+    }
+  }
+
+  getRoute (query) {
+    return this.routes.find((route)=> {
+      return Object.entries(query).every(([k, v])=> {
+        return (route[k] === v);
+      });
+    });
+  }
+
+  // match
+  // -----
+  // Checks whether there is a route matching the passed pathname
+  // If there is a match, returns the associated Page and matched params.
+  // If no match return NotFound
+  match (input) {
+    let {url, route} = input;
+
+    let {session} = this;
+
+    let match = null;
+    for (const r of this.routes) {
+      match = r.match({url, route, session});
+      if (match) {
+        break;
+      }
+    }
+
+    if (!match) {
+      match = new MatchResult({
+        input,
+        notFound: true,
+        redirect: 'notFound'
+      });
+    }
+
+    let {redirect} = match;
+    if (redirect) {
+      if (!(redirect in this.redirects)) {
+        throw new Error(`Missing redirect for ${redirect}`);
+      }
+      match.redirect = this.redirects[redirect];
+      match.url = match.redirect.buildUrl();
+      this._go(match.url);
+    }
+
+    return match;
+  }
+
+  onChange (listener) {
+    this.listeners.push(listener);
+  }
+
+  go ({url, route}) {
+    let match = this.match({url, route});
+    this._go(match.url);
+    for (let listener of this.listeners) {
+      listener(match.url);
+    }
+  }
+
+  _go (url) {
+    if (typeof window !== 'undefined') {
+      window.history.pushState({}, '', url);
+    }
+  }
+}
+
+module.exports = Router;
